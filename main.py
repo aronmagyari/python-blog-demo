e39087138af4325746a06b9c4aa66ab2534cb7b7
Ferencz Magyari <afmagyari@gmail.com>
2016-10-13 14:36:49 -0700

    simplified hello world

--- main.py
+++ main.py
@@ -1,18 +1,9 @@
-from flask import Flask
-app = Flask(__name__)
-app.config['DEBUG'] = True
+import webapp2
 
-# Note: We don't need to call run() since our application is embedded within
-# the App Engine WSGI application server.
+class BlogApp(webapp2.RequestHandler):
+    def get(self):
+        self.response.write('Hello, blog!')
 
-
-@app.route('/')
-def hello():
-    """Return a friendly HTTP greeting."""
-    return 'Hello World!'
-
-
-@app.errorhandler(404)
-def page_not_found(e):
-    """Return a custom 404 error."""
-    return 'Sorry, nothing at this URL.', 404
+app = webapp2.WSGIApplication([
+    ('/', BlogApp),
+], debug = True)
