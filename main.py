3992eb5718e3e1a607e28f7f35fbdfd014443396
Ferencz Magyari <afmagyari@gmail.com>
2016-10-13 16:47:03 -0700

    added new post feature

--- main.py
+++ main.py
@@ -3,9 +3,25 @@ import os
 import webapp2
 import jinja2
 
+from google.appengine.ext import db
+
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
                                autoescape = True)
+
+def blog_key(name = 'default'):
+    return db.Key.from_path('blogs', name)
+
+class Post(db.Model):
+    subject = db.StringProperty(required = True)
+    content = db.TextProperty(required = True)
+    created = db.DateTimeProperty(auto_now_add = True)
+    last_modified = db.DateTimeProperty(auto_now = True)
+
+    def render(self):
+        self._render_text = self.content.replace('\n', '<br>')
+        return render_str("post.html", p = self)
+
 
 class BlogHandler(webapp2.RequestHandler):
     def write(self, *a, **kw):
@@ -21,6 +37,30 @@ class BlogHandler(webapp2.RequestHandler):
     def get(self):
         self.render('base.html')
 
+class BlogFront(BlogHandler):
+    def get(self):
+        posts = greetings = Post.all().order('-created')
+        self.render('front.html', posts = posts)
+
+class NewPost(BlogHandler):
+    def get(self):
+        self.render("newpost.html")
+
+    def post(self):
+
+        subject = self.request.get('subject')
+        content = self.request.get('content')
+
+        if subject and content:
+            p = Post(parent = blog_key(), subject = subject, content = content)
+            p.put()
+            self.redirect('/blog/%s' % str(p.key().id()))
+        else:
+            error = "Include subject and content, please!"
+            self.render("newpost.html", subject=subject, content=content, error=error)
+
+
 app = webapp2.WSGIApplication([
-    ('/', BlogHandler),
+    ('/', BlogFront),
+    ('/newpost', NewPost)
 ], debug = True)
