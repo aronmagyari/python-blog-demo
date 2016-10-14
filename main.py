6e13bfb7a59b967f04f708c18614a54c4edd6777
Ferencz Magyari <afmagyari@gmail.com>
2016-10-13 18:02:47 -0700

    created signup page

--- main.py
+++ main.py
@@ -21,6 +21,33 @@ class Post(db.Model):
     def _render_text(self):
         return self.content.replace('\n', '<br>')
 
+class User(db.Model):
+    name = db.StringProperty(required = True)
+    pw_hash = db.StringProperty(required = True)
+    email = db.StringProperty()
+
+    @classmethod
+    def by_id(cls, uid):
+        return User.get_by_id(uid, parent = users_key())
+
+    @classmethod
+    def by_name(cls, name):
+        u = User.all().filter('name =', name).get()
+        return u
+
+    @classmethod
+    def register(cls, name, pw, email = None):
+        pw_hash = make_pw_hash(name, pw)
+        return User(parent = users_key(),
+                    name = name,
+                    pw_hash = pw_hash,
+                    email = email)
+
+    @classmethod
+    def login(cls, name, pw):
+        u = cls.by_name(name)
+        if u and valid_pw(name, pw, u.pw_hash):
+            return u
 
 class BlogHandler(webapp2.RequestHandler):
     def write(self, *a, **kw):
@@ -69,8 +96,14 @@ class PostPage(BlogHandler):
 
         self.render("post.html", p = post)
 
+class Signup(BlogHandler):
+    def get(self):
+        self.render("signup.html")
+
+
 app = webapp2.WSGIApplication([
     ('/', BlogFront),
     ('/newpost', NewPost),
-    ('/blog/([0-9]+)', PostPage)
+    ('/blog/([0-9]+)', PostPage),
+    ('/signup', Signup)
 ], debug = True)
